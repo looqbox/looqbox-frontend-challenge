1f519614c6188ddf19643a46c404240ebcb0a2cb
Pedro Muraki <pedromuraki@gmail.com>
2019-05-12 18:07:57 -0300

    Tested 'getEvolutions'

--- challenge/src/js/tests/helpers/helpers.test.js
+++ challenge/src/js/tests/helpers/helpers.test.js
@@ -105,3 +105,56 @@ describe('Test "getRandomNumber"', () => {
     expect(getRandomNumber(50, 75)).to.be.above(49).and.to.be.below(76)
   })
 })
+
+/* GET EVOLUTIONS */
+describe('Test "getEvolutions"', () => {
+  /* Bulbasaur evolution chain (Pokémon that evolves) */
+  const bulbasaurChain = {
+    species: {
+      name: 'bulbasaur',
+      url: 'https://pokeapi.co/api/v2/pokemon-species/1/'
+    },
+    evolves_to: [
+      {
+        species: {
+          name: 'ivysaur',
+          url: 'https://pokeapi.co/api/v2/pokemon-species/2/'
+        },
+        evolves_to: [
+          {
+            species: {
+              name: 'venusaur',
+              url: 'https://pokeapi.co/api/v2/pokemon-species/3/'
+            },
+            evolves_to: []
+          }
+        ]
+      }
+    ]
+  }
+
+  /* Mewtwo evolution chain (Pokémon that doesn't evolve) */
+  const mewtwoChain = {
+    species: {
+      name: 'mewtwo',
+      url: 'https://pokeapi.co/api/v2/pokemon-species/150/'
+    },
+    evolves_to: []
+  }
+
+  it('Should return an array if Pokémon evolves (if first "evolves_to" array in chain is not empty)', () => {
+    expect(getEvolutions(bulbasaurChain)).to.be.an('array')
+  })
+
+  it('Should return null if Pokémon doesn\'t evolves (if first "evolves_to" array in chain is empty)', () => {
+    expect(getEvolutions(mewtwoChain)).to.be.equal(null)
+  })
+
+  it('Should return an array (if Pokémon evolves) with each evolution chain name', () => {
+    expect(getEvolutions(bulbasaurChain)).to.be.deep.equal(['bulbasaur', 'ivysaur', 'venusaur'])
+  })
+
+  it('Should stop the recursion if "stop" parameter is "true" and return the actual "evolutions" array', () => {
+    expect(getEvolutions(bulbasaurChain, ['bulbasaur', 'ivysaur'], true)).to.be.deep.equal(['bulbasaur', 'ivysaur'])
+  })
+})
