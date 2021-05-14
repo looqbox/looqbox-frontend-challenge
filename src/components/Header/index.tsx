4e68c4bd1538fdbcb078c2f59fa59ade90d98078
Luan Miqueias <luan987654321@hotmail.com>
2021-05-14 14:39:59 -0300

    Criando component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+import icon_search from '../../assets/icons/search.svg';
+const Header = () => {
+    return (
+        <header className={styles.container}>
+            <div className={styles.content}>
+                <h1>Search for a Pokemon</h1>
+                <form className={styles.search_form}>
+                    <label htmlFor="search">
+                        <input
+                            type="text"
+                            name="search"
+                            autoComplete="search"
+                            id="search"
+                            placeholder="Bulbassar"
+                        />
+                        <button type="submit" className={styles.button_search}>
+                            <img
+                                src={icon_search}
+                                alt="Pesquisar"
+                                width="100%"
+                                height="100%"
+                            />
+                        </button>
+                    </label>
+                </form>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Header from '../../components/Header';
 import styles from './styles.module.css';
 
 export const Home = () => {
-    return <div className={styles.container}>Home</div>;
+    return (
+        <div className={styles.container}>
+            <Header />
+        </div>
+    );
 };
