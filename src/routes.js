3639d27e981c7daba20c541946588081caee2d54
Mateus Fugita <mateushiwao@gmail.com>
2021-05-15 17:45:17 -0300

    fix routes error

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Details from './pages/Details';
 
 function Routes(){
     return (
         <BrowserRouter>
             <Switch>
-                <Route path='' exact component={Home} />
+                <Route path='/' exact component={Home} />
+                <Route path='/details/:id' exact component={Details} />
             </Switch>
         </BrowserRouter>
     )
