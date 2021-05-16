409acf55c20b4d4e45909fb5530839c8217161f6
Luan Miqueias <luan987654321@hotmail.com>
2021-05-16 13:33:57 -0300

    Create loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const Loading: React.FC = () => {
+    return <div className={styles.loading}></div>;
+};
+
+export default Loading;
