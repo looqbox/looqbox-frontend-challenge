bf2d3b33ca38bc0f7264d310648718ee7bc445c6
Juliana Santana <juliana.c.s9898@gmail.com>
2024-03-08 19:50:35 -0300

    chore: config router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import HomePage from './views/Home/index.tsx';
+import DetailsPage from './views/Details/index.tsx';
+import NotFoundPage from './views/NotFound/index.tsx';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>,
+    errorElement: <NotFoundPage />
+  },
+  {
+    path: '/details',
+    element: <DetailsPage/>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/views/Details/index.tsx
@@ -0,0 +1,7 @@
+export default function DetailsPage() {
+    return (
+        <div>
+            <h1>Details Page</h1>
+        </div>
+    )
+}
