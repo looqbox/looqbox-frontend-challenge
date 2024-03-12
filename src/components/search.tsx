83d535fbc0ed5cda73a418b632ca38db2fa93816
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-12 00:43:29 -0300

    feat: add pokemon search via form

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,22 +1,54 @@
-import { FormEvent } from 'react'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { z } from 'zod'
 
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 
-export const Search = () => {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+const searchForm = z.object({
+  search: z.string(),
+})
 
-    console.log(e.target)
+type SearchFormTypes = z.infer<typeof searchForm>
+
+export const Search = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm()
+
+  const navigate = useNavigate()
+
+  const handleSearch = async (data: SearchFormTypes) => {
+    if (data.search.trim() !== '') {
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${data.search.toLocaleLowerCase()}`
+      )
+
+      const finds = await res.json()
+
+      if (finds) {
+        navigate(`/pokemon/${finds.name}`)
+      }
+    }
   }
 
   return (
     <form
       className="flex w-full max-w-96 gap-4"
-      onSubmit={(e) => handleSubmit(e)}
+      onSubmit={handleSubmit(handleSearch)}
     >
-      <Input className="w-full border-white" />
-      <Button className="text-white">Submit</Button>
+      <Input
+        className="w-full border-white"
+        type="search"
+        placeholder="Search for pokémon; e.g. Lucario"
+        {...register('search')}
+      />
+
+      <Button disabled={isSubmitting} className="text-white">
+        Submit
+      </Button>
     </form>
   )
 }
