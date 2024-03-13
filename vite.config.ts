d6201a52dd875f129cf2d40a135dd9139490a8a5
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-13 11:08:23 -0300

    style: applies Prettier formatation

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import path from "path"
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 
   server: {
     port: 3000,
-    open: true
-  }
+    open: true,
+  },
 })
