da8eb51a04444235f0601e2c20196729aa77ff27
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-13 17:54:15 -0300

    tests: setup testing environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, InlineConfig, UserConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +15,12 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
+
+  test: {
+    globals: true, // para não precisar importar as funções do pacote "vitest" em todos os arquivos.
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
