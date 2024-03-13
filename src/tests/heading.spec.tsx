9b64929deb672cf5f36b0f30ddbf66065a381a0a
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-13 18:44:52 -0300

    test: finish heading tests

--- src/tests/heading.spec.tsx
+++ src/tests/heading.spec.tsx
@@ -1,3 +1,29 @@
-test('1 + 1 equals 2', () => {
-  expect(1 + 1).toEqual(2)
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { Heading } from '@/components/heading'
+
+describe('Heading', () => {
+  it('should display the right text', () => {
+    const wrapper = render(
+      <MemoryRouter>
+        <Heading />
+      </MemoryRouter>
+    )
+
+    const statusText = wrapper.getByText('National Pokedex')
+    expect(statusText).toBeInTheDocument()
+  })
+
+  it('should have an working anchor', () => {
+    const wrapper = render(
+      <MemoryRouter>
+        <Heading />
+      </MemoryRouter>
+    )
+
+    const statusText = wrapper.getByTestId('link')
+    expect(statusText).toBeInTheDocument()
+    expect(statusText).toHaveAttribute('href', '/')
+  })
 })
