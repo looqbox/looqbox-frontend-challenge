a1e1c9ec4451cfd31c346828235209564359650c
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-13 11:09:26 -0300

    perf(loader): set static height and width to image

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -2,7 +2,7 @@ import loader from '../assets/loader.svg'
 export const Loader = () => {
   return (
     <div className="flex items-center justify-center">
-      <img src={loader} alt="" />
+      <img width={60} height={60} src={loader} alt="" />
     </div>
   )
 }
