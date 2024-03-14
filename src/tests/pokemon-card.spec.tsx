c5ad9ddbe7403c481bcc7b30278e3997bd5b2f90
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-14 18:56:21 -0300

    test(pokemon-card): add unit test

---
+++ src/tests/pokemon-card.spec.tsx
@@ -0,0 +1,49 @@
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { PokemonCard } from '@/components/home/pokemon-card'
+
+describe('Pokémon card', () => {
+  it('should render', () => {
+    const testData = {
+      id: 1,
+      sprites: {
+        other: {
+          home: {
+            front_default:
+              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+          },
+        },
+      },
+
+      name: 'bulbasaur',
+      order: 1,
+
+      types: [
+        {
+          slot: 1,
+          type: {
+            name: 'grass',
+            url: 'https://pokeapi.co/api/v2/type/12/',
+          },
+        },
+        {
+          slot: 2,
+          type: {
+            name: 'poison',
+            url: 'https://pokeapi.co/api/v2/type/4/',
+          },
+        },
+      ],
+    }
+
+    const wrapper = render(
+      <MemoryRouter>
+        <PokemonCard data={testData} />
+      </MemoryRouter>
+    )
+
+    const text = wrapper.getByText('bulbasaur')
+    expect(text).toBeInTheDocument()
+  })
+})
