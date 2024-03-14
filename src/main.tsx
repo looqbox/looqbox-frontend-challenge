6ea0a1eb47f0cb0fc0436d4c93dddc8af36c2fd0
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-14 17:31:02 -0300

    fix(build): handle Vercel's error regarding file naming

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import './index.css'
 
 import ReactDOM from 'react-dom/client'
 
-import App from './app.tsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
