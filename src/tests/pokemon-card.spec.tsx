c1971df030522f4221531b7ada9ed989d0e7bbc9
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-14 21:10:38 -0300

    test(pokemon-card): add unit test

--- src/tests/pokemon-card.spec.tsx
+++ src/tests/pokemon-card.spec.tsx
@@ -46,4 +46,47 @@ describe('Pokémon card', () => {
     const text = wrapper.getByText('bulbasaur')
     expect(text).toBeInTheDocument()
   })
+
+  it('should have a link generated correctly', () => {
+    const testData = {
+      id: 1,
+      sprites: {
+        other: {
+          home: {
+            front_default:
+              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+          },
+        },
+      },
+
+      name: 'bulbasaur',
+      order: 1,
+
+      types: [
+        {
+          slot: 1,
+          type: {
+            name: 'grass',
+            url: 'https://pokeapi.co/api/v2/type/12/',
+          },
+        },
+        {
+          slot: 2,
+          type: {
+            name: 'poison',
+            url: 'https://pokeapi.co/api/v2/type/4/',
+          },
+        },
+      ],
+    }
+
+    const wrapper = render(
+      <MemoryRouter>
+        <PokemonCard data={testData} />
+      </MemoryRouter>
+    )
+
+    const text = wrapper.getByTestId('pokemon-link')
+    expect(text).toHaveAttribute('href', '/pokemon/1')
+  })
 })
