9b4e77eb525c564c2f7562abffc3d51168d34414
Sidnei Farias <sidneijr.77@gmail.com>
2024-03-14 23:09:08 -0300

    fix: remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
-import { Toaster } from './components/ui/sonner'
 import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
