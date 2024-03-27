064cafc66aefa973941c624fc460b1c2cf67e941
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-03-27 18:06:00 -0300

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Layout } from "antd";
+const { Header } = Layout;
+
+const HeaderStyle: React.CSSProperties = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "#5c4c92",
+};
+
+const CustomHeader: React.FC = () => (
+  <Header style={HeaderStyle}>
+    <img
+      src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png"
+      alt="Pokédex"
+      height={50}
+    />
+  </Header>
+);
+
+export default CustomHeader;
