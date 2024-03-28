c0e6b1543df86f06b14fd4e8e4c95edfe3337e9e
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-03-28 18:07:09 -0300

    Update Home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import { Layout, Typography, Input } from "antd";
-import { useMediaQuery } from "react-responsive";
+import React, { useEffect } from "react";
+import { Layout, Typography, Input, Grid } from "antd";
+import { useDispatch } from "react-redux";
 import CustomHeader from "../components/Header";
+import PokemonsList from "../components/PokemonsList";
+import { fetchPokemons } from "../store/pokemonSlicer";
+import { AppDispatch } from "../store";
+import CustomPagination from "../components/Pagination";
+
 const { Content } = Layout;
 const { Title, Text } = Typography;
 const { Search } = Input;
+const { useBreakpoint } = Grid;
 
 const ContentStyle: React.CSSProperties = {
   display: "flex",
@@ -12,13 +18,18 @@ const ContentStyle: React.CSSProperties = {
   minHeight: "calc(100vh - 64px)",
   alignItems: "center",
   textAlign: "center",
-  backgroundColor: "#fff",
+  backgroundColor: "E0E2DB",
   padding: "40px 18px",
-  color: "#fff",
 };
 
 const Home: React.FC = () => {
-  const isDesktop = useMediaQuery({ query: "(min-width: 992px)" });
+  const dispatch = useDispatch<AppDispatch>();
+  const screens = useBreakpoint();
+  const isDesktop = screens.md;
+
+  useEffect(() => {
+    dispatch(fetchPokemons(0));
+  }, []);
 
   return (
     <Layout>
@@ -26,18 +37,24 @@ const Home: React.FC = () => {
       <Content style={ContentStyle}>
         <Title>Welcome to Pokédex!</Title>
         <Text style={{ marginBottom: "24px" }}>
-          Simply enter a Pokémon's name or number in the search bar below to
-          begin your journey.
+          Enter a Pokémon's name or number in the search bar below to begin your
+          journey.
         </Text>
         <Search
-          style={{ width: `${isDesktop ? "50%" : "100%"}` }}
+          style={{
+            width: `${isDesktop ? "50%" : "100%"}`,
+            marginBottom: "40px",
+          }}
           placeholder="Pokémon's name or number"
           enterButton="Search"
           size="large"
           allowClear
         />
+        <PokemonsList />
+        <CustomPagination />
       </Content>
     </Layout>
   );
 };
+
 export default Home;
