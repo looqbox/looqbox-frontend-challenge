2febca248f245c75402f3fb64e6809ef4745c76f
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-03-28 16:18:30 -0300

    Add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,12 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: {
+          loader: "file-loader",
+        },
+      },
     ],
   },
   resolve: {
