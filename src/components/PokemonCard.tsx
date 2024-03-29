457588d8e96fa47f5af348c5e4dec38c348c9a97
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-03-28 23:38:18 -0300

    Style adjustments

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import { Card, Image, Typography, Flex } from "antd";
+import emptyImg from "../../assets/no-pictures.png";
 
 const { Title, Text } = Typography;
 
-export interface PokemonBaseInfo {
+export type PokemonBaseInfo = {
   character: {
     id: number;
     name: string;
   };
   isDesktop?: boolean;
-}
+};
 
 const PokemonCard: React.FC<PokemonBaseInfo> = ({ character, isDesktop }) => {
   const { id, name } = character;
   return (
-    <Card style={{ width: isDesktop ? 300 : "90vw" }} hoverable>
+    <Card
+      style={{
+        width: isDesktop ? 300 : "90vw",
+        minHeight: "100%",
+        textAlign: "center",
+      }}
+      hoverable
+    >
       <Image
         preview={false}
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        fallback={emptyImg}
       />
-      <Flex justify="center" align="center" gap="small">
-        <Text type="secondary">#{id}</Text>
+      <Flex justify="center" align="center" vertical>
+        <Text type="secondary" style={{ marginTop: "8px" }}>
+          #{id}
+        </Text>
         <Title
-          level={3}
+          level={4}
           style={{
             margin: "0",
             textTransform: "capitalize",
