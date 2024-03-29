9981ef947518c1146b7d2c046da72235884e14f8
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-03-29 10:03:42 -0300

    Create link to Pokemon page

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, Image, Typography, Flex } from "antd";
+import { Link } from "react-router-dom";
 import emptyImg from "../../assets/no-pictures.png";
 
 const { Title, Text } = Typography;
@@ -15,36 +16,38 @@ export type PokemonBaseInfo = {
 const PokemonCard: React.FC<PokemonBaseInfo> = ({ character, isDesktop }) => {
   const { id, name } = character;
   return (
-    <Card
-      style={{
-        width: isDesktop ? 300 : "90vw",
-        minHeight: "100%",
-        textAlign: "center",
-      }}
-      hoverable
-    >
-      <Image
-        preview={false}
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-        fallback={emptyImg}
-      />
-      <Flex justify="center" align="center" vertical>
-        <Text type="secondary" style={{ marginTop: "8px" }}>
-          #{id}
-        </Text>
-        <Title
-          level={4}
-          style={{
-            margin: "0",
-            textTransform: "capitalize",
-            fontWeight: 700,
-            color: "#515052",
-          }}
-        >
-          {name}
-        </Title>
-      </Flex>
-    </Card>
+    <Link to={`/pokemon/${name}`}>
+      <Card
+        style={{
+          width: isDesktop ? 300 : "90vw",
+          minHeight: "100%",
+          textAlign: "center",
+        }}
+        hoverable
+      >
+        <Image
+          preview={false}
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+          fallback={emptyImg}
+        />
+        <Flex justify="center" align="center" vertical>
+          <Text type="secondary" style={{ marginTop: "8px" }}>
+            #{id}
+          </Text>
+          <Title
+            level={4}
+            style={{
+              margin: "0",
+              textTransform: "capitalize",
+              fontWeight: 700,
+              color: "#515052",
+            }}
+          >
+            {name}
+          </Title>
+        </Flex>
+      </Card>
+    </Link>
   );
 };
 
