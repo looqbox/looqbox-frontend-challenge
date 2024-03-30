6b0adf7303796daf91dda41ae37caddb6f953d52
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-03-30 00:35:43 -0300

    Create Stats component

---
+++ src/routes/Character/Stats.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Typography, Flex, Grid } from "antd";
+import getStatsList from "../../utils/getStatsList";
+
+type StatsProps = {
+  stats: Array<{
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }>;
+};
+
+const { Text, Title } = Typography;
+const { useBreakpoint } = Grid;
+
+const Stats: React.FC<StatsProps> = ({ stats }) => {
+  const statsList = getStatsList(stats);
+  const screens = useBreakpoint();
+  const isDesktop = screens.md;
+
+  return (
+    <div className="stats-wrapper">
+      <Title level={3}>Base Stats</Title>
+
+      {statsList.map((stat) => (
+        <>
+          <Text type="secondary">{stat.name.toUpperCase()}</Text>
+          <Flex>
+            <div
+              className="stat-range"
+              style={{
+                width: `calc(${stat.value}px * ${isDesktop ? "5" : "3"})`,
+              }}
+            />
+            <Text strong>{stat.value}</Text>
+          </Flex>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Stats;
