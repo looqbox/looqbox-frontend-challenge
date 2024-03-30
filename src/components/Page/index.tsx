e9ab6d2c7bdc030980e60ce8216c2536a53a5f09
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-03-30 14:41:58 -0300

    Add footer

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import { Layout } from "antd";
 import CustomHeader from "../Header";
+import CustomFooter from "../Footer";
 import "./styles.css";
 
 const { Content } = Layout;
@@ -13,6 +14,7 @@ const Page: React.FC<PageProps> = ({ children }) => (
   <Layout>
     <CustomHeader />
     <Content className="content">{children}</Content>
+    <CustomFooter />
   </Layout>
 );
 export default Page;
