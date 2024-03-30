7f90d36545fd30ad0efd13a8d81fd88d76ecf393
Roberta Quiles <roberta@MacBook-Pro-de-Roberta.local>
2024-03-30 00:38:19 -0300

    Create InfoCard component

---
+++ src/routes/Character/InfoCard.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Typography, Flex } from "antd";
+import getAbilities from "../../utils/getAttributesList";
+import "./styles.css";
+import { Pokemon } from "./types";
+
+const { Text } = Typography;
+
+const InfoCard: React.FC<Pokemon> = ({ character }) => {
+  const { height, weight, abilities } = character;
+  const abilitiesList = getAbilities(abilities, "ability");
+
+  return (
+    <div className="info-card">
+      <Flex className="info-group">
+        <Text type="secondary">Height: </Text>
+        <Text strong className="info-text">
+          {height}
+        </Text>
+      </Flex>
+
+      <Flex className="info-group">
+        <Text type="secondary">Weight: </Text>
+        <Text strong className="info-text">
+          {weight}
+        </Text>
+      </Flex>
+
+      <Flex className="info-group">
+        <Text type="secondary">Abilities: </Text>
+        <div className="info-text">
+          {abilitiesList.map((ability, index) => (
+            <Text strong key={ability}>
+              {index === abilitiesList.length - 1 ? ability : `${ability}, `}
+            </Text>
+          ))}
+        </div>
+      </Flex>
+    </div>
+  );
+};
+
+export default InfoCard;
