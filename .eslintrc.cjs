00ab73c7708730a1efe9ac6226cb555d211fa427
Larissa Rabelo <larissarabelolf@gmail.com>
2024-04-07 20:36:09 -0300

    chore(app): install and config service with axios and react query

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,12 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
     "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "@tanstack/query"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
