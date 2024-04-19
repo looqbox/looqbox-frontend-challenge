8457452877ea52e33a0b73157823a79656a77e09
Bruno Souza <brunoezully@gmail.com>
2024-04-19 20:17:46 -0300

    chore: setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        card: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;",
+      },
+    },
+  },
+  plugins: [],
+};
