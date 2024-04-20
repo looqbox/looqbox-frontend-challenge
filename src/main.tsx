450747d1994a06d5b76c666d0131b464262c8da3
Bruno Souza <brunoezully@gmail.com>
2024-04-20 14:46:45 -0300

    build: add @tanstack/react-query package and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,23 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { PokemonContextProvider } from "./contexts/PokemonContextProvider.tsx";
 import { routes } from "./routes/routes.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 10, // 10 minutes
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <PokemonContextProvider>
-      <RouterProvider router={routes} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={routes} />
+      </QueryClientProvider>
     </PokemonContextProvider>
   </React.StrictMode>
 );
