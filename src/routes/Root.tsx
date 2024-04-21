3540e30e8159c1a2cf701166a6503d55206f9168
Bruno Souza <brunoezully@gmail.com>
2024-04-20 23:56:20 -0300

    feat: add Logo component to Root.tsx

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Menu from "../components/Menu/Menu";
 import Footer from "../components/Footer";
+import Logo from "../components/UI/Logo";
 
 function Root() {
   return (
     <div className="flex flex-col h-full w-full">
       <header className="w-full px-2">
+        <Logo />
         <Menu />
       </header>
       <Outlet />
