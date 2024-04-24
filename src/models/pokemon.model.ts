0681a95c688695d15dcc88baceb238695deff49d
Guilherme Almeida <guica.almeida@gmail.com>
2024-04-23 23:23:23 -0300

    feat: create first render of pokeons in cards

---
+++ src/models/pokemon.model.ts
@@ -0,0 +1,50 @@
+export default interface Pokemon {
+    abilities: Ability[];
+    base_experience: number;
+    cries: Cries;
+    game_indices: Gameindex[];
+    height: number;
+    id: number;
+    name: string;
+    sprites: Sprites;
+    stats: Stat[];
+    types: Type[];
+    weight: number;
+  }
+  
+  interface Type {
+    slot: number;
+    type: ReferenceToEndpoint;
+  }
+  
+  interface Stat {
+    base_stat: number;
+    effort: number;
+    stat: ReferenceToEndpoint;
+  }
+  
+  interface Sprites {
+    back_default: string;
+    front_default: string;
+  }
+  
+  interface Gameindex {
+    game_index: number;
+    version: ReferenceToEndpoint;
+  }
+  
+  interface Cries {
+    latest: string;
+  }
+  
+  interface Ability {
+    ability: ReferenceToEndpoint;
+    is_hidden: boolean;
+    slot: number;
+  }
+  
+  interface ReferenceToEndpoint {
+    name: string;
+    url: string;
+  }
+  
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,46 @@
+import { Card, Flex, Input, Layout } from "antd";
+import { useEffect, useState } from "react";
+import Pokemon from "../models/pokemon.model";
+const { Search } = Input;
+const { Content } = Layout;
+
 export default function Home() {
-  return <h1>hi</h1>;
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/").then(
+        (res) => res.json()
+      );
+      setPokemons(response.results);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Layout>
+      <Content style={{ maxWidth: "1200px", margin: "0 auto" }}>
+        <Search
+          placeholder="input search text"
+          enterButton="Search"
+          size="large"
+        />
+        <Flex wrap="wrap" justify="center" gap="24px">
+          {pokemons?.map((poke) => {
+            return (
+              <Card
+                title={poke.name}
+                extra={poke.id}
+                key={poke.id}
+                style={{ width: "376px" }}
+              >
+                <p>{poke.name}</p>
+                <p>{poke.base_experience}</p>
+              </Card>
+            );
+          })}
+        </Flex>
+      </Content>
+    </Layout>
+  );
 }
