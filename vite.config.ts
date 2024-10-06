78837b3ab25d16eceaa47127f2ded47a25b20b7b
silvestr3 <fellipe.silvestre@outlook.com>
2024-10-06 20:45:03 -0300

    test: Add unit tests for card

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from "vite";
+import path from "node:path";
+
 import react from "@vitejs/plugin-react";
-import path from "path";
+import type { UserConfig } from "vite";
+import { defineConfig } from "vite";
+import type { InlineConfig } from "vitest/node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src/"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: "./test/setup.ts",
+    environment: "happy-dom",
+  },
+} as UserConfig & {
+  test: InlineConfig;
 });
