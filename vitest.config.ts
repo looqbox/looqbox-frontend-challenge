77fa0184e2c499d66471607ddaac4a97a6791d15
CaueFidelis <cauefidelis@live.com>
2026-01-23 18:02:36 -0300

    first commit

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setupTests.ts'],
+    globals: true,
+    css: true,
+  },
+});
