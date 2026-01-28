f885968d4da73990ecf0d952243e6743450863d8
CaueFidelis <cauefidelis@live.com>
2026-01-28 18:01:46 -0300

    chore: setup global styles, fonts and app metadata

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,21 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './app/routes/Router.tsx';
 import { Provider } from 'react-redux';
 import { store } from './app/store/store.ts';
+import { ConfigProvider } from 'antd';
+import './styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily: 'Nunito, system-ui, sans-serif',
+          },
+        }}
+      >
+        <RouterProvider router={router} />
+      </ConfigProvider>
     </Provider>
   </StrictMode>,
 );
